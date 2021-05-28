7a27200680a5225b8bfa17495082d247cf939f83
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-05-27 22:37:00 -0400

    redux & react

--- src/app.js
+++ src/app.js
@@ -2,4 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+
+const store = configureStore()
+
+store.dispatch(addExpense({ description: 'Water Bill' }))
+store.dispatch(addExpense({ description: 'Gas Bill' }))
+store.dispatch(setTextFilter('gas'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses);
+
+
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
